2f4886faee5ff361f257ddd0d2a0f682fcb05416
Josh Goebel <me@joshgoebel.com>
2018-01-06 05:42:04 -0500

    wip

--- worker/hooks.js
+++ worker/hooks.js
@@ -1,20 +1,28 @@
 Module.last_tick = 0;
+Module.count=0;
 Module.ready = function () {
   var d = new Date();
   var now = d.getTime();
-  if (Module.count> 1000) {
+  if (Module.count> 10) {
     blow_up();
   }
   if (now - Module.last_tick > 1000) {
-    console.log("returning 1");
+    // console.log("returning 1");
     Module.count += 1;
     Module.last_tick = now;
+    postMessage("do frame [" + Module.count + "]");
     return 1;
   }
   else {
-    console.log("returning 0");
+    // console.log("returning 0");
     return 0;
   }
 }
+
+onmessage = function(e) {
+  console.log("received " + e.data);
+}
+
+console.log("booter");
 
 // var myWorker = new Worker('worker.js');
--- worker/output.cpp
+++ worker/output.cpp
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <emscripten.h>
 
+int count = 0;
+
 bool ready() {
   int ready;
   ready = EM_ASM_INT({
   return Module.ready();
 },0);
-printf("%d",ready);
+// printf("%d",ready);
   return ready;
 }
 
 void loop() {
   if (ready()) {
-    printf("hello\n");
+    count++;
+    printf("hello %d\n", count);
   }
 }
 
